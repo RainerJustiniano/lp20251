a2ba3f9700bd08323bbb625096f19c61c4b1db2b
Rainer Justiniano <rainer.j@estudante.ifmt.edu.br>
2025-07-13 16:22:29 +0000

    atu

---
+++ testeprova.py
@@ -0,0 +1,18 @@
+1. Faça um programa que gere aleatoriamente 15 números inteiros em uma lista
+#   e depois permita que o usuário digite um número inteiro para ser buscado
+#   na lista, se for encontrado o programa deve imprimir a posição desse
+#   número na lista, caso contrário, deve imprimir a mensagem: "Não encontrado!".
+#   (2,5pt)
+
+2. Complemente a questão 1 sem utilizar o recurso de ordenação. O programa
+#   deve  imprimir ao final: o maior e o menor número da lista, o percentual
+#   de números pares e a média dos elementos da lista. (2,5pt)
+
+3. Altere a questão 1 ou 2 da prova, para que os 15 números da lista não sejam
+#   mais aleatórios, e sim provenientes de um arquivo de nome "entrada.csv", cuja
+#   primeira e única linha do arquivo, contenha os 15 números separados por
+#   ponto e vírgula (;). (2,5pt)
+
+4. Complemente a questão 1, 2 ou 3 da prova, para que o resultado apresentado
+#   em tela, seja gravado em um arquivo de nome "resultado.txt". (2,5pt)
+ 
